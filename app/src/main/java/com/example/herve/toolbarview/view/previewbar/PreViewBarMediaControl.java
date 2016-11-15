8e8a3fe935d743a31433c30301edd49c78cdd9b9
lijianyou-Herve <lijianyou.herve@gmail.com>
2016-11-15 18:07:14 +0800

    优化刻度

---
+++ app/src/main/java/com/example/herve/toolbarview/view/previewbar/PreViewBarMediaControl.java
@@ -0,0 +1,59 @@
+package com.example.herve.toolbarview.view.previewbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+import com.example.herve.toolbarview.view.ijkplayer.widget.media.AndroidMediaController;
+
+/**
+ * Created           :Herve on 2016/11/15.
+ *
+ * @ Author          :Herve
+ * @ e-mail          :lijianyou.herve@gmail.com
+ * @ LastEdit        :2016/11/15
+ * @ projectName     :ToolBarView
+ * @ version
+ */
+public class PreViewBarMediaControl extends AndroidMediaController {
+
+
+    public PreViewBarMediaControl(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PreViewBarMediaControl(Context context, boolean useFastForward) {
+        super(context, useFastForward);
+    }
+
+    public PreViewBarMediaControl(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void doPauseResume() {
+        if (mPlayer.isPlaying()) {
+            if (onStateListener != null) {
+                onStateListener.onStateListener(false);
+            }
+        } else {
+            if (onStateListener != null) {
+                onStateListener.onStateListener(true);
+            }
+        }
+        super.doPauseResume();
+
+    }
+
+
+    private OnStateListener onStateListener;
+
+    public void setOnStateListener(OnStateListener onStateListener) {
+        this.onStateListener = onStateListener;
+    }
+
+    public interface OnStateListener {
+
+        void onStateListener(boolean isPlaying);
+    }
+}
